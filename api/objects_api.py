7518ed60bd19186707aad2d96cd843ac7c891a4c
Dmitry Vlasov <d.vlasov@hh.ru>
2024-05-28 03:29:42 +0200

    add api modules

---
+++ api/objects_api.py
@@ -0,0 +1,10 @@
+from api import routes
+from api.api_client import ApiClient
+
+
+def get_users(client: ApiClient, gender: str):
+    return client.get(routes.Routes.USERS, params={'gender': gender} if gender else None)
+
+
+def get_user(client: ApiClient, user_id: str):
+    return client.get(routes.Routes.USER.format(user_id))
---
+++ api/routes.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class Routes(StrEnum):
+    USERS = '/users'
+    USER = '/user/{}'
